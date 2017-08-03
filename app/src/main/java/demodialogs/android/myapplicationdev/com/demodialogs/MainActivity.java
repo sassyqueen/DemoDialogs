bd2c839ffc750f7e512faf318c766d86698d22c9
15004557 <H0peyur3gd7>
2017-08-03 10:25:15 +0800

    with different dialogs

--- app/src/main/java/demodialogs/android/myapplicationdev/com/demodialogs/MainActivity.java
+++ app/src/main/java/demodialogs/android/myapplicationdev/com/demodialogs/MainActivity.java
@@ -1,11 +1,15 @@
 package demodialogs.android.myapplicationdev.com.demodialogs;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -74,6 +78,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         } else if (item.getItemId() == R.id.itemPassPhrase) {
+
+            LayoutInflater inflater = (LayoutInflater)
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout passPhrase =
+                    (LinearLayout) inflater.inflate(R.layout.passphrase, null);
+            final EditText etPassphrase = (EditText) passPhrase
+                    .findViewById(R.id.editTextPassPhrase);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Please Enter")
+                    .setView(passPhrase)
+                    .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(MainActivity.this, "You had entered " +
+                                    etPassphrase.getText().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
         }
 
 
